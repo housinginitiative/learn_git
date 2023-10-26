041f66c672794248013961a382285a1d80aee68e
Chi-Hyun <139492649+chihyunkim@users.noreply.github.com>
2023-10-26 17:25:03 -0400

    Add initial file

---
+++ scripts/start_here.R
@@ -0,0 +1,48 @@
+# Purpose -------------------------------------------------------------------------------------
+# To help folks learn how to use git, github, and version control workflows to ensure
+# project reproducibility and smooth collaborations.
+
+# Preliminaries -------------------------------------------------------------------------------
+
+# All of the packages below are highly recommended to be installed. 
+# Package usethis is necessary for setup.
+library(tidyverse)
+library(tidylog)
+library(here)
+library(janitor)
+library(scales)
+library(readxl)
+library(skimr)
+library(usethis)
+
+here::i_am("scripts/start_here.R")
+
+# Set up Box path -----------------------------------------------------------------------------
+
+# Only code and aggregated analysis products are to be synced to Github.
+# Data must be stored on Box, because they can contain sensitive or privacy-protected information. 
+# If you have not already installed the Box desktop application, please do that before continuing.
+
+# To refer to files stored on Box, we need to have some way to refer to varying path names
+# because the path that points to our Box installations is different on each machine.
+
+# To store your own Box path, 
+#   1) Uncomment and run the following line of code, which should open the Renviron file.
+#                 usethis::edit_r_environ()
+#   2) Add the following line to the Renviron file, save, then restart RStudio. 
+#     PATH should be replaced with your own machine's path to the top-level Box directory,
+#     for example: /Users/USERNAME/Library/CloudStorage/Box-Box
+#                 BOX_PATH = "PATH"
+
+# Examine your Renviron file again, and you should see your Box path. 
+# This only needs to be done once per machine.
+
+# Now, the following should show you the path of the test data file.
+data_path = str_c(Sys.getenv("BOX_PATH"),
+                  "/Learn git example data/LIHTCPUB.CSV")
+
+# Now you should have some delicious LIHTC data.
+data_test <- read_csv(data_path, guess_max = Inf)
+
+
+
